dccee2e55f19bc7be0b9254ef405b4455758f77c
amin <amin1382422amin@gmail.com>
2025-02-05 16:55:31 +0330

    added Convention for flag

---
+++ src/Infrastructure/Persistance/Configuration/Settings/SettingConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Shifty.Domain.Common.BaseClasses;
+using Shifty.Domain.Features.Setting;
+
+namespace Shifty.Persistence.Configuration.Settings;
+
+public class SettingConfiguration    : IEntityTypeConfiguration<Setting>
+{
+    public void Configure(EntityTypeBuilder<Setting> builder)
+    {
+        builder.Property(a=>a.Flags)
+               .HasConversion<long>();
+    }
+}
