dc667fac8bd7b5ebc4a2630b38192697fa528079
amin <amin1382422amin@gmail.com>
2025-02-06 18:58:44 +0330

    mig

---
+++ src/Infrastructure/Persistance/Migrations/Application/20250205132740_sdfsdfsdf.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shifty.Persistence.Migrations.Application
+{
+    /// <inheritdoc />
+    public partial class sdfsdfsdf : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Flags",
+                table: "Settings",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Flags",
+                table: "Settings",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
