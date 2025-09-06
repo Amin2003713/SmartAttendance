8e9d46e496c68045ab4bd9c686a1d67112e8b3b7
Nima <nima.bt.1611@gmail.com>
2025-09-06 16:57:39 +0330

    migration

---
+++ src/Infrastructure/Persistance/Migrations/20250906090708_ForTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shifty.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ForTest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CalendarProjects");
+
+            migrationBuilder.CreateTable(
+                name: "Vehicles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PlateNumber_LeftNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PlateNumber_MiddleMark = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PlateNumber_RightNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PlateNumber_RegionCode = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DeletedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicles", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vehicles");
+
+            migrationBuilder.CreateTable(
+                name: "CalendarProjects",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CalendarId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DeletedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CalendarProjects", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CalendarProjects_DailyCalendars_CalendarId",
+                        column: x => x.CalendarId,
+                        principalTable: "DailyCalendars",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CalendarProjects_CalendarId",
+                table: "CalendarProjects",
+                column: "CalendarId");
+        }
+    }
+}
---
+++ src/Infrastructure/Persistance/Migrations/20250906110616_test.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shifty.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class test : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Brand",
+                table: "Vehicles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Descriprion",
+                table: "Vehicles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Model",
+                table: "Vehicles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ResponsibleId",
+                table: "Vehicles",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<byte>(
+                name: "Status",
+                table: "Vehicles",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0);
+
+            migrationBuilder.AddColumn<byte>(
+                name: "VehicleType",
+                table: "Vehicles",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "YearOfProduction",
+                table: "Vehicles",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Brand",
+                table: "Vehicles");
+
+            migrationBuilder.DropColumn(
+                name: "Descriprion",
+                table: "Vehicles");
+
+            migrationBuilder.DropColumn(
+                name: "Model",
+                table: "Vehicles");
+
+            migrationBuilder.DropColumn(
+                name: "ResponsibleId",
+                table: "Vehicles");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Vehicles");
+
+            migrationBuilder.DropColumn(
+                name: "VehicleType",
+                table: "Vehicles");
+
+            migrationBuilder.DropColumn(
+                name: "YearOfProduction",
+                table: "Vehicles");
+        }
+    }
+}
