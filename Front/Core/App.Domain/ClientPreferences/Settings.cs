e5105df38881291181ff257dcbbc249ae6898d51
amin <amin1382422amin@gmail.com>
2025-09-20 08:12:49 +0330

    remove client prefrences

--- Front/Core/App.Domain/ClientPreferences/Settings.cs
+++
@@ -1,9 +0,0 @@
-namespace App.Domain.ClientPreferences;
-
-public class Settings
-{
-    public int Id { get; set; }
-    public bool IsDarkMode { get; set; }
-    public bool IsRTL { get; set; } = true;
-    public string LanguageCode { get; set; } = "fa-Ir";
-}
